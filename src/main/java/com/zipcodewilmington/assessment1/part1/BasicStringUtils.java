b3ff85b11da357a2c67f347f8b10035995ae3194
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-17 09:38:10 -0500

    finished more tests

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -18,18 +18,20 @@ public class BasicStringUtils {
      * @param str string input from client
      * @return string with identical contents, in the reverse order
      */
-//    public static String reverse(String str) {
-//        StringBuilder str1 = new StringBuilder(str);
-//        str1.reverse();
-//        return str1.toString();
-//    }
+    public static String reverse(String str) {
+        StringBuilder str1 = new StringBuilder(str);
+        StringBuilder reverseStr1 = str1.reverse();
+        return reverseStr1.toString();
+    }
 
     /**
      * @param str string input from client
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        String reverse = reverse(str);
+        String reverseCamel = camelCase(reverse);
+        return reverseCamel;
     }
 
 
@@ -38,7 +40,7 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1, str.length() -1);
     }
 
     /**
@@ -46,6 +48,16 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if ('A' <= array[i] && array[i] <= 'Z'){
+                array[i] += 32;
+            }
+            else if ('a' <= array[i] && array[i] <= 'z'){
+                array[i] -= 32;
+            }
+        }
+        String results = new String(array);
+        return results;
     }
 }
